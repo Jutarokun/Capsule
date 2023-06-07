43435af1b2bb5cf8d66564057221ab36fcea70d1
Jesse <jesseryan@bluewin.ch>
2023-06-07 21:19:08 +0200

    worked on database register function

--- database.js
+++ database.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
 // Open the database connection
-let db = new sqlite3.Database('database.db', (err) => {
+let db = new sqlite3.Database('./database/database.db', (err) => {
   if (err) {
     console.error(err.message);
   }
@@ -11,26 +11,65 @@ let db = new sqlite3.Database('database.db', (err) => {
 });
 
 function register_user(username, email, password) {
-    let searchUsername = `SELECT COUNT(*) FROM users WHERE username = ${username}`;
-    let searchEmail = `SELECT COUNT(*) FROM users WHERE username = ${email}`;
-    let hashedPassword;
-    if (db.run(searchUsername) > 0) {
-        return 'Username was already picked please enter another username';
-    } else if (db.run(searchEmail) > 0) {
-        return 'Email is already picked please enter another email';
+  let query = `SELECT username FROM user WHERE username = ?`;
+  let query2 = `SELECT email FROM user WHERE email = ?`;
+  let checkValue = username;
+  let checkValue2 = email;
+  let username_exists = false;
+  let email_exists = false;
+  let message;
+
+  db.get(query, [checkValue], (err, row) => {
+    if (err) {
+      console.error(err.message);
+      // Handle the error appropriately
+      return;
+    } else if (row) {
+      console.log(`${checkValue} is in the database.`);
+      username_exists = true;
+      message = `${checkValue} is in the database.`;
     } else {
-        bcrypt.hash(password, 10, function (err, hash) {
-            if (err) {
-              console.log('error while hashing password');
-            } else {
-              // Store the hash in the database or wherever you need
-              console.log('Hashed password:', hash);
-              hashedPassword = hash;
-            }
-          });
-        db.run(`INSERT INTO user (username, email, password) VALUES (${username}, ${email}, ${hashedPassword})`)
+      console.log(`${checkValue} is not in the database.`);
+      message = `${checkValue} is not in the database.`;
     }
+
+    if (!email_exists) {
+      db.get(query2, [checkValue2], (err, row) => {
+        if (err) {
+          console.error(err.message);
+          // Handle the error appropriately
+          message = err.message;
+        } else if (row) {
+          console.log(`${checkValue2} is in the database.`);
+          email_exists = true;
+          message = `${checkValue2} is in the database.`;
+        } else {
+          console.log(`${checkValue2} is not in the database.`);
+          message = `${checkValue2} is not in the database.`;
+        }
+        if (username_exists && email_exists) {
+          console.log('aaaaaaaaaaaaaaaaaaaaaaaaaaaaa');
+          message = 'Username and Email are already picked please enter other values or try to log in';
+        }
+
+        if (!username_exists && !email_exists) {
+          let hashedPassword = bcrypt.hashSync(password, saltRounds);
+          db.run(`INSERT INTO user (username, email, password) VALUES (?, ?, ?)`, [username, email, hashedPassword], (err) => {
+            if (err) {
+              console.error(err.message);
+              // Handle the error appropriately
+              message = err.message;
+            }
+            console.log('Inserted into database');
+            message = 'Inserted into database';
+          });
+        }
+      });
+    }
+    return message;
+  });
 }
+
 
 module.exports = {
   register_user
--- index.js
+++ index.js
@@ -1,38 +1,52 @@
 const express = require('express');
 const app = express();
 const server = require('http').createServer(app); // Create an HTTP server instance
+const io = require('socket.io')(server); // Initialize Socket.IO with the server
 const bodyParser = require("body-parser");
 const db = require('./database.js');
 
 // set resources to express static
 app.use(express.static('public/css'));
-
 app.use('/assets', express.static('public/assets'));
-
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Serve static files (HTML, CSS, JavaScript) from a directory
 app.use(express.static('public'));
 
 // Start the server
-server.listen(3000, () => {
-  console.log('Server is running on port 3000');
+const port = 3000; // or any other desired port number
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
 
-// set handler for initial request
+// Handle HTTP GET request for the root URL ("/")
 app.get('/', (req, res) => {
   res.sendFile(__dirname + "/public/registration.html");
-})
+});
 
-app.post('/registration', (req, res) => {
+// Handle HTTP POST request to the "/registration" endpoint
+app.post('/registration', async (req, res) => {
   let username = req.body.username;
   let email = req.body.email;
   let password = req.body.password;
-  let check = db.register_user(username, email, password);
-  console.log(check);
-  res.redirect('/login');
-})
+  let result = db.register_user(username, email, password);
+  console.log('endresult: ' + result);
+});
 
+// Handle HTTP GET request to the "/login" endpoint
 app.get('/login', (req, res) => {
   res.sendFile(__dirname + "/public/login.html");
-})
+});
+
+// Handle Socket.IO connections
+io.on('connection', (socket) => {
+  console.log('A client connected');
+
+  // Handle client disconnection
+  socket.on('disconnect', () => {
+    console.log('A client disconnected');
+  });
+
+  // Handle custom events
+  // ...
+});
