23f1d4990a9c1786b91b9fd8259964cc83f453f5
Jutarokun <jesseryan@bluewin.ch>
2023-06-09 13:31:56 +0200

    worked on creating pods / rooms

--- public/js/capsule.js
+++ public/js/capsule.js
@@ -1,25 +1,2 @@
-let button = document.getElementById('addCapsule');
-const socket = io('http://localhost:8080');
-
-button.addEventListener('click', () => {
+document.addEventListener('DOMContentLoaded', () => {
 })
-
-
-// Emit a "createCapsule" event
-socket.emit('createCapsule', { name, description });
-
-// Listen for a "capsuleCreated" event from the server
-socket.on('capsuleCreated', (capsule) => {
-  // Handle the created capsule data
-  console.log('Capsule created:', capsule);
-});
-
-
-// Emit a "joinCapsule" event
-socket.emit('joinCapsule', capsuleId);
-
-// Listen for a "capsuleJoined" event from the server
-socket.on('capsuleJoined', (capsule) => {
-  // Handle the joined capsule data
-  console.log('Capsule joined:', capsule);
-});
